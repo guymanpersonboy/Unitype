bbec9a514a8714e900302dbe09c4a1741e198581
Christopher Carrasco <chris.carrasco@att.net>
2024-06-10 22:38:21 -0500

    Added Prefabs and Scripts folders

---
+++ Assets/Scripts/Typer.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Typer : MonoBehaviour
+{
+    // TODO need a word bank
+    public Text wordOutput = null;
+
+    private string remainingWord = string.Empty;
+    private string currentWord = "muffins";
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        SetCurrentWord();
+    }
+
+    private void SetCurrentWord()
+    {
+        // TODO get bank word
+        SetRemainingWord(currentWord)
+    }
+
+    private void SetRemainingWord(string newString)
+    {
+        remainingWord = newString;
+        wordOutput.text = newString;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        CheckInput();
+    }
+    private void CheckInput()
+    {
+        if (Input.anyKeyDown)
+        {
+            string keysPressed = Input.inputString;
+
+            if (keysPressed.Length == 1)
+                { EnterLetter(keysPressed); }
+        }
+    }
+
+    private void EnterLetter(string typedLetter)
+    {
+        if (IsCorrectLetter(typedLetter))
+        {
+            RemoveLetter();
+
+            if (IsWordComplete())
+                { SetCurrentWord(); }
+        }
+    }
+
+    private bool IsCorrectLetter(string letter)
+    {
+        return remainingWord.IndexOf(letter) == 0;
+    }
+
+    private void RemoveLetter()
+    [
+        string newString = remainingWord.Remove(0, 1);
+        SetRemainingWord(newString);
+    ]
+
+    private bool IsWordComplete()
+    {
+        return remainingWord.Length == 0;
+    }
+}
